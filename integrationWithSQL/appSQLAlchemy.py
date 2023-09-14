4afe4474fb5bc6ae139cdf9063b731e823e64969
Renan de Souza <rdsrenansouza@gmail.com>
2023-09-14 06:49:23 -0300

    Create date base and insertion data

--- integrationWithSQL/appSQLAlchemy.py
+++ integrationWithSQL/appSQLAlchemy.py
@@ -2,27 +2,51 @@ from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 from sqlalchemy import ForeignKey
 from sqlalchemy import Table, Column, Integer, String, DECIMAL
+from sqlalchemy import insert
 
-
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+engine = create_engine("sqlite+pysqlite:///sqlite.db", echo=True)
 metadata_obj = MetaData()
 
 client_table = Table(
-                "clients",
-                metadata_obj,
-                Column("id", Integer, primary_key=True, autoincrement=True),
-                Column("name", String(30)),
-                Column("cpf", String(11), unique=True),
-                Column("address", String(50), unique=True)
-            )
+    "clients",
+    metadata_obj,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("name", String(30)),
+    Column("cpf", String(11), unique=True),
+    Column("address", String(50), unique=True)
+)
 
 account_table = Table(
-    "address",
+    "account",
     metadata_obj,
-    Column("id", primary_key=True, autoincrement=True),
+    Column("id", Integer, primary_key=True, autoincrement=True),
     Column("type", String),
     Column("agency", Integer),
     Column("number", Integer),
     Column("balance", DECIMAL),
     Column("client_id", ForeignKey("clients.id"), nullable=False)
 )
+
+with engine.begin() as conn:
+    metadata_obj.create_all(conn)
+
+# Usar metodo metada para popular banco ou mudar para declarative base
+josias_client = insert(client_table).values(name='Josias Afam', cpf='02548974556',
+                                            address='Rua Ingá, 80 - Canoas/RS')
+josias_account = insert(account_table).values(type='Conta Corrente', agency=1001,
+                                              number=100203, balance=300.00, client_id=1)
+malaquias_client = insert(client_table).values(name='Malaquias Kron', cpf='02358654721',
+                                               address='Rua Logo Ali, 9990 - Longe/RS')
+malaquias_account = insert(account_table).values(type='Conta Corrente', agency=1001,
+                                                 number=100201, balance=548.54, client_id=1)
+
+with engine.connect() as conn:
+    result = conn.execute(josias_client)
+    print(result)
+    result = conn.execute(josias_account)
+    print(result)
+    result = conn.execute(malaquias_client)
+    print(result)
+    result = conn.execute(malaquias_account)
+    print(result)
+    conn.commit()
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
-Parte 1 - Implementando um Banco de Dados Relacional com SQLAlchemy
+# Desafio DIO.COM - Formação Python Developer
 
-Objetivo:
+## _Integrando Python com SQLite e MongoDB_
+
+### _Parte 1 - Implementando um Banco de Dados Relacional com SQLAlchemy_
 
 Neste desafio você irá implementar uma aplicação de integração com SQLite com base em um esquema relacional disponibilizado.
 Sendo assim, utilize o esquema dentro do contexto de cliente e conta para criar as classes de sua API. Essas classes
@@ -8,5 +10,19 @@ irão representar as tabelas do banco de dados relacional dentro de aplicação.
 
 IMAGEM
 
-Entregável:
+Entregáveis:
 
+- Aplicação com a definição do esquema por meio das classes usando SQLAlchemy
+- Inserção de um conjunto de dados mínimo para manipulação das informações
+- Execução de métodos de recuperação de dados via SQLAlchemy
+
+### _Parte 2 – Implementando um Banco de Dados NoSQL com Pymongo_
+
+Você irá implementar um banco NoSQL com mongodb para fornecer uma visão agregada do modelo relacional. Sendo assim, as informações de cliente e contas existentes estão contidas dentro de documentos de acordo com cliente.
+
+Execute as operações:
+
+- Conecte ao mongo atlas e crie um banco de dados
+- Defina uma coleção bank para criar os documetos de clientes
+- Insira documentos com a estrutura mencionada
+- Escreve instruções de recuperação de informações com base nos pares de chave e valor como feito em aula
