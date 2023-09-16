6c402eecdcfda63ecc9c30bca3ff89d487792903
Renan de Souza <rdsrenansouza@gmail.com>
2023-09-16 01:01:45 -0300

    Finaly SQLalchemy

--- integrationWithSQL/appSQLAlchemy.py
+++ integrationWithSQL/appSQLAlchemy.py
@@ -1,42 +1,48 @@
 from sqlalchemy import create_engine
-from sqlalchemy import MetaData
-from sqlalchemy import ForeignKey
-from sqlalchemy import Table, Column, Integer, String, DECIMAL
-from sqlalchemy import insert
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, Integer, String, DECIMAL, ForeignKey
+from sqlalchemy import text
 from faker import Faker
 
 engine = create_engine("sqlite+pysqlite:///sqlite.db", echo=True)
-metadata_obj = MetaData()
+Base = declarative_base()
 fake = Faker(["pt_BR"])
 
-client_table = Table(
-    "clients",
-    metadata_obj,
-    Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("name", String(30)),
-    Column("cpf", String(11), unique=True),
-    Column("address", String(50), unique=True)
-)
 
-account_table = Table(
-    "accounts",
-    metadata_obj,
-    Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("type", String),
-    Column("agency", Integer),
-    Column("number", Integer),
-    Column("balance", DECIMAL),
-    Column("client_id", ForeignKey("clients.id"), nullable=False)
-)
+class Client(Base):
+    __tablename__ = 'clients'
 
-with engine.begin() as conn:
-    metadata_obj.create_all(conn)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(30))
+    cpf = Column(String(11), unique=True)
+    address = Column(String(50), unique=True)
 
-with engine.connect() as conn:
 
+class Account(Base):
+    __tablename__ = 'accounts'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(String)
+    agency = Column(Integer)
+    number = Column(Integer)
+    balance = Column(DECIMAL)
+    client_id = Column(ForeignKey("clients.id"), nullable=False)
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+
+with Session.begin() as session:
     for i in range(1000):
-        conn.execute(insert(client_table).values(name=fake.name(), cpf=fake.cpf(),
-                                                 address=fake.address()))
-        conn.execute(insert(account_table).values(type='Conta Corrente', agency=1001,
-                                                  number=str(fake.aba()), balance=0.0, client_id=i+1))
-    conn.commit()
+        session.add(Client(name=fake.name(), cpf=fake.cpf(), address=fake.address()))
+        session.add(Account(type='Conta Corrente', agency=1001, number=str(fake.aba()), balance=0.0, client_id=i))
+
+with Session.begin() as session:
+    stmt = text('SELECT * FROM clients c INNER JOIN accounts a ON a.client_id = c.id')
+    clients = session.execute(stmt)
+
+for client_list in clients:
+    address = client_list[3].replace("\n", " - ")
+    print(f'Nome: {client_list[1]} - CPF: {client_list[2]} - Endereço: {address}'
+          f' - Tipo de conta: {client_list[5]} - Banco: {client_list[6]} - Número da conta: {client_list[7]}'
+          f' - Saldo: R$ {client_list[8]}')
